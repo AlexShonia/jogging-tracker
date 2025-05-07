478eee22804ddde057a32eff17dc07bbfcb73e6a
AlexShonia <alex.shonia123@gmail.com>
2025-05-07 15:27:27 +0400

    update settings

--- website/website/settings.py
+++ website/website/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-cutsdt)9&%*@9x70o8i%xua0bm6lzxjw#c2*b#k8iqqfn*#!xx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
